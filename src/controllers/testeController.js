e32fed30b1f5847058ded65ca89c384c4f6638e7
Bruno Noguchi <bruno.noguchi@db1.com.br>
2023-01-26 16:57:29 -0300

    algumas mudanças

--- src/controllers/testeController.js
+++ src/controllers/testeController.js
@@ -1,50 +1,51 @@
+const crypto = require('crypto-js/sha256');
+
 const obterFormasDePagamento = (request, response) => {
+  const { comboId } = request.params;
+
   return response.status(200).json([
     {
       id: 2,
-      formaDePagamentoId: 2,
-      meioDePagamento: 'A VISTA',
-      formaDePagamento: 'BOLETO BANCÁRIO',
+      formaDePagamento: 'A VISTA',
+      meioDePagamento: 'BOLETO BANCÁRIO',
       parcelas: [
         {
-          numeroParcela: 1,
-          valor: 199.8,
+          numeroDeParcelas: 1,
+          valorDaParcela: 199.8,
         },
       ],
     },
     {
-      id: 2,
-      formaDePagamentoId: 4,
-      meioDePagamento: 'A VISTA OU PARCELADO',
-      formaDePagamento: 'CARTÃO DE CRÉDITO',
+      id: 4,
+      formaDePagamento: 'A VISTA OU PARCELADO',
+      meioDePagamento: 'CARTÃO DE CRÉDITO',
       parcelas: [
         {
-          numeroParcela: 1,
-          valor: 199.8,
+          numeroDeParcelas: 1,
+          valorDaParcela: 199.8,
         },
         {
-          numeroParcela: 2,
-          valor: 99.9,
+          numeroDeParcelas: 2,
+          valorDaParcela: 99.9,
         },
         {
-          numeroParcela: 3,
-          valor: 66.6,
+          numeroDeParcelas: 3,
+          valorDaParcela: 66.6,
         },
       ],
     },
     {
-      id: 2,
-      formaDePagamentoId: 9,
-      meioDePagamento: 'PAGAMENTO RECORRENTE',
-      formaDePagamento: 'CARTÃO DE CRÉDITO',
+      id: 9,
+      formaDePagamento: 'PAGAMENTO RECORRENTE',
+      meioDePagamento: 'CARTÃO DE CRÉDITO',
       parcelas: [
         {
-          numeroParcela: 1,
-          valor: 199.8,
+          numeroDeParcelas: 1,
+          valorDaParcela: 199.8,
         },
         {
-          numeroParcela: 2,
-          valor: 99.9,
+          numeroDeParcelas: 2,
+          valorDaParcela: 99.9,
         },
       ],
     },
@@ -55,7 +56,18 @@ const validarInadiplenciaDoResponsavel = (request, response) => {
   return response.status(200).send(false);
 };
 
+const gerarIdentificador = (request, response) => {
+  const { comboId, pessoaId, dataHora, colaboradorId } = request.query;
+  // const { comboId, pessoaId, dataHora, colaboradorId } = request.params;
+  // const { comboId, pessoaId, dataHora, colaboradorId } = request.body;
+
+  const hash = crypto(comboId + pessoaId + dataHora + colaboradorId);
+
+  return response.status(200).json(hash.toString());
+};
+
 module.exports = {
   obterFormasDePagamento,
   validarInadiplenciaDoResponsavel,
+  gerarIdentificador,
 };
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const testeController = require('./controllers/testeController');
 const router = express.Router();
 
 router.get('/api/AssinaturaDeComboDeCurso/Combo/:comboId/FormasDePagamento', testeController.obterFormasDePagamento);
-router.get('/api/AssinaturaDeComboDeCurso/validarInadiplencia/:numeroDocumentoResponsavel', testeController.validarInadiplenciaDoResponsavel);
+router.get('/api/AssinaturaDeComboDeCurso/Combo/:comboId/:pessoaId/:tipoPessoa/ValidarInadimplencia/', testeController.validarInadiplenciaDoResponsavel);
+router.get('/api/AssinaturaDeComboDeCurso/Combo/GerarIdentificadorUnico/', testeController.gerarIdentificador);
 
 module.exports = router;
